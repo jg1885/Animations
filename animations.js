3c522ee722a12537e8e62aac88c5f9d3af8eed09
JG <jgsk8core@gmail.com>
2019-02-01 14:11:09 -0500

    2/1/19 Update

--- animations.js
+++ animations.js
@@ -1,19 +1,52 @@
-//Toggling between One element i.e., Expanding text & hiding it
+//START OPEN CLOSE TOGGLE BUTTON
 
 $(document).ready(function() {
 
 
-
-  //Reveal Text/toggle
-
-
   $(".button").click(function() {
-
-    //wrapping the if/ else statemetn inside the text
+    //wrapping the if/ else statement inside the text
     $(this).text($(this).text() == 'View More +' ? 'View Less -' : 'View More +');
     $(this).toggleClass("animation");
     $(".text").stop().toggle("fast", "linear");
 
  });
 
+//END OPEN / CLOSE BUTTON
+
+
+$(".red-square-button").click(function(){
+location.reload(true);
 });
+
+});
+
+
+
+
+//vanilla JS & HTML5 canvas
+
+
+
+// START NEW CANVAS RANDOM RED SQUARES
+
+function draw() {
+  var canvas = document.getElementById('red-squares');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+
+    for (var i =0; i < 20; i++) {
+      var x = Math.random() * canvas.width;
+      var y = Math.random() * canvas.height;
+         x= Math.floor(x) + 1;
+         y= Math.floor(y) + 1;
+         console.log(x, y);
+    ctx.beginPath;
+    ctx.moveTo(0,0);
+    ctx.fillStyle="red";
+    ctx.fillRect (x, y, 15, 15);
+
+    }
+  }
+
+      }
